1fd5e8a88ed8362fa584f4aa5a3acc95c4f1911b
athiragi <athirasnair72476@gmail.com>
2022-05-19 21:43:15 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import Friend from './component/Friend';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-       <Friend/>
+       <BrowserRouter>
+       <Routes>
+         <Route path='/' exact element={<Friend/>}/>
+
+       </Routes>
+       </BrowserRouter>
     </div>
   );
 }
